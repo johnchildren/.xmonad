7a7a09655e8b3ffbf2be13da548791e8d01a893e
John Children <john@infosum.com>
2018-03-27 17:11:00 +0100

    initial commit

---
+++ xmonad.hs
@@ -0,0 +1,28 @@
+import           XMonad
+import           XMonad.Config.Desktop (desktopConfig)
+import           XMonad.Util.EZConfig
+
+myTerminal = "alacritty -e tmux"
+
+myModMask = mod4Mask
+
+myNormalBorderColor = "#002b36"
+myFocusedBorderColor = "#657b83"
+
+myWorkspaces = ["one", "two", "three", "four", "five"]
+
+myRemoveKeys = [ (myModMask .|. shiftMask, xK_q) -- leaving xmonad in lxqt is annoying
+               ]
+
+myAdditionalKeys = [ ((myModMask .|. shiftMask, xK_l), spawn "lxqt-leave --lockscreen")
+                   , ((myModMask .|. shiftMask, xK_q), spawn "lxqt-leave --logout")
+                   ]
+
+defaults = desktopConfig { terminal           = myTerminal
+                         , modMask            = myModMask
+                         , normalBorderColor  = myNormalBorderColor
+                         , focusedBorderColor = myFocusedBorderColor
+                         , workspaces         = myWorkspaces
+                         } `removeKeys` myRemoveKeys `additionalKeys` myAdditionalKeys
+
+main = xmonad defaults
